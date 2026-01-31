98eae35b49ccc8f48a25c34816738241dede703c
arimartzbrito25 <arimartzbrito25@gmail.com>
2026-01-31 15:37:17 -0600

    Initialize React + Vite shopping cart project
    Add initial project structure for a shopping cart app using React and Vite. Includes configuration files, static assets, main HTML, and basic components.

---
+++ Carrito-compras/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './componentes/Header'
+import Footer from './componentes/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header/>
+        <Footer/>
+      
+    </>
+  )
+}
+
+export default App
