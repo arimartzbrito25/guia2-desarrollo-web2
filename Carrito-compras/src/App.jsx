4a4288adf295e65e74c5377b38e66dfacfd2b8bf
arimartzbrito25 <arimartzbrito25@gmail.com>
2026-02-01 13:17:48 -0600

    Refactor to React app structure and add Guitar component
    Replaces static HTML in index.html with a React root and script. Refactors main content into React components, introduces a new Guitar component, and updates App.jsx to render a sample guitar collection using the new component. Footer and Header components are cleaned up for React usage.

--- Carrito-compras/src/App.jsx
+++ Carrito-compras/src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './componentes/Header'
 import Footer from './componentes/Footer'
+import Guitar from './componentes/Guitar'
+import { db } from './data/db'
+
+
+
 
 const App = () => {
+
+  const [data, setData]=useState(db);
+  console.log(data);
+
   return (
     <>
-        <Header/>
-        <Footer/>
-      
+      <Header />
+      <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+          <Guitar nombre="Guitarra 1"/>
+          <Guitar  nombre="Guitarra 2"/>
+          <Guitar  nombre="Guitarra 3"/>
+
+        </div>
+      </main>
+      <Footer />
+
     </>
   )
 }
